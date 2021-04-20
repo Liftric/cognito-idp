e0f9f6368469d35e131e6903bb5eab5b28f5507b
Jan Gaebel <g.jan94@icloud.com>
2021-04-20 14:50:22 +0200

    fix(gradle): make properties also optional

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -155,8 +155,8 @@ publishing {
 }
 
 signing {
-    val signingKey: String by project
-    val signingPassword: String by project
+    val signingKey: String? by project
+    val signingPassword: String? by project
     useInMemoryPgpKeys(signingKey, signingPassword)
     sign(publishing.publications)
 }
