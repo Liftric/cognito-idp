59668b841adfaa735c57c92f979dd295528602ef
Marcel Kesselring <2410681-Ingwersaft@users.noreply.gitlab.com>
2021-06-02 12:24:25 +0200

    feat(AuthHandlerJSTest): add initial test case

---
+++ auth/src/jsTest/kotlin/com/liftric/auth/AuthHandlerJSTest.kt
@@ -0,0 +1,90 @@
+package com.liftric.auth
+
+import AuthHandlerJS
+import com.liftric.auth.base.Region
+import com.liftric.auth.base.UserAttribute
+import env
+import io.ktor.http.*
+import kotlinx.coroutines.await
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+class AuthHandlerJSTest {
+    // private val configuration = Configuration(
+    //        env["origin"] ?: "",
+    //        Region.euCentral1,
+    //        env["clientid"] ?: ""
+    //    )
+    private val authHandler = AuthHandlerJS(
+        env["origin"] ?: error("missing origin"),
+        Region.euCentral1.code,
+        env["clientid"] ?: error("missing origin")
+    )
+
+    // Randomize temp user account name to not exceed aws try threshold
+    private val random = (0..999).random()
+    private val username = "auth-lib-test-user-${random}"
+    private val password = "auth-lib-test-user-${random}A1@"
+
+
+    data class Credentials(val username: String, val password: String)
+
+    private fun randomUser(): Credentials {
+        val random = (0..9999).random()
+        return Credentials(
+            username = "auth-lib-test-user-${random}",
+            password = "auth-lib-test-user-${random}A1@"
+        )
+    }
+
+    private suspend fun createUser(): Pair<String, Credentials> {
+        val credential = randomUser()
+        val userAttributes: List<UserAttribute> = listOf(
+            UserAttribute(
+                Name = "custom:target_group",
+                Value = "ROLE_PATIENT"
+            )
+        )
+        val signUpResponse = authHandler.signUp(credential.username, credential.password, userAttributes.toTypedArray())
+
+        assertNull(signUpResponse.await())
+        assertNotNull(signUpResponse.await())
+
+        val signInResponse = authHandler.signIn(credential.username, credential.password)
+        assertNotNull(signInResponse.await())
+
+        return Pair(signInResponse.await().AuthenticationResult.AccessToken, credential)
+    }
+
+    private suspend fun deleteUser(token: String) {
+        val deleteUserResponse = authHandler.deleteUser(token)
+        assertNull(deleteUserResponse.await())
+    }
+
+    @JsName("SignUpSignInDeleteUserTest")
+    @Test
+    fun `Sign up, sign in, delete user should succeed`() = runTest {
+        println("Sign up, sign in, delete user should succeed")
+        authHandler.signUp(
+            username, password,
+            attributes = arrayOf(
+                UserAttribute(Name = "custom:target_group", Value = "ROLE_USER")
+            )
+        ).await().also {
+            println("signUpResponse=$it")
+            assertNotNull(it)
+        }
+
+        authHandler.signIn(username, password).await().also {
+            println("signInResponse=$it")
+            assertNotNull(it)
+
+            authHandler.deleteUser(it.AuthenticationResult.AccessToken).await().also {
+                println("deleteUser=$it")
+                assertNotNull(it)
+            }
+        }
+    }
+}
