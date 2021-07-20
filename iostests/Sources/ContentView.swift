7d693cfde954e171e18af16b6fe5d2e494543a78
Jan Gaebel <g.jan94@icloud.com>
2021-07-20 15:28:38 +0200

    feat(cognito-idp): create ios test app to check if requests can be made

---
+++ iostests/Sources/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Jan Gaebel on 20.07.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ iostests/Sources/Request.swift
@@ -0,0 +1,23 @@
+//
+//  Request.swift
+//  TestApp
+//
+//  Created by Jan Gaebel on 20.07.21.
+//
+
+import Foundation
+import cognito_idp
+
+
+public class Provider {
+    var request: IdentityProviderClient = {
+        let process = ProcessInfo.processInfo
+        guard let region = process.environment["region"] else {
+            fatalError("Missing region")
+        }
+        guard let clientId = process.environment["clientId"] else {
+            fatalError("Missing clientId")
+        }
+        return IdentityProviderClient(region: region, clientId: clientId)
+    }()
+}
