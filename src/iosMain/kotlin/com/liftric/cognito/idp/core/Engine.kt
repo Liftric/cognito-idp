62d3164c677c2093c468699d63e851603101dff4
Jan Gaebel <gaebel.dev@icloud.com>
2022-11-03 18:26:48 +0700

    refactor(gradle): use legacy darwin engine

--- src/iosMain/kotlin/com/liftric/cognito/idp/core/Engine.kt
+++ src/iosMain/kotlin/com/liftric/cognito/idp/core/Engine.kt
@@ -1,7 +1,7 @@
 package com.liftric.cognito.idp.core
 
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.darwin.Darwin
+import io.ktor.client.engine.darwin.DarwinLegacy
 
-internal actual val Engine: HttpClientEngine = Darwin.create()
+internal actual val Engine: HttpClientEngine = DarwinLegacy.create()
 
