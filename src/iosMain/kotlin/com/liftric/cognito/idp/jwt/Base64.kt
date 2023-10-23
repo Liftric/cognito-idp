d360bb7a321112c543cd972be56eaf7937933891
Marcel Kesselring <2410681-Ingwersaft@users.noreply.gitlab.com>
2023-10-23 11:11:38 +0200

    chore(iosMain/Base64): use unsigned zero for `NSData.create` call

--- src/iosMain/kotlin/com/liftric/cognito/idp/jwt/Base64.kt
+++ src/iosMain/kotlin/com/liftric/cognito/idp/jwt/Base64.kt
@@ -13,7 +13,7 @@ internal actual class Base64 {
             if (remainder > 0) {
                 encoded64 = encoded64.padEnd(input.count() + (4 - remainder), '=')
             }
-            return NSData.create(encoded64, 0)?.let {
+            return NSData.create(encoded64, 0u)?.let {
                 @Suppress("USELESS_CAST") // false positive inspection: konan fails if the cast is removed
                 (NSString.create(it, NSUTF8StringEncoding) as String?)
             }?: run {
