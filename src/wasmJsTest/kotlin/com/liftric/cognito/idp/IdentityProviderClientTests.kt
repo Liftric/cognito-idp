1cba25b998c5e0847fe7a6e14a39b7183b2e17b9
EpicSquid <epicsquid315@gmail.com>
2025-05-19 12:27:26 +1000

    Added tests matching ios

---
+++ src/wasmJsTest/kotlin/com/liftric/cognito/idp/IdentityProviderClientTests.kt
@@ -0,0 +1,12 @@
+package com.liftric.cognito.idp
+
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.promise
+
+actual class IdentityProviderClientTests : AbstractIdentityProviderClientTests()
+
+actual fun runTest(block: suspend () -> Unit) {
+	MainScope().promise {
+		block.invoke()
+	}
+}
