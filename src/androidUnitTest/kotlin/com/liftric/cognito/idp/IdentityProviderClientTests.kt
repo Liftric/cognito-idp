bbf6eea6598c1cf6eb27bcc2a844462d0ea2384f
Marc Alexander Voigt <voigt@liftric.com>
2025-10-16 13:55:15 +0200

    feat(dependencies): update dependencies
    update gradle to 9.1
    update to ktor 3
    update to kotlin 2.2
    raise min android sdk version
    raise java target
    update dependencies
    replace deprecations in build.gradle.kts
    replace setup from old dependencies
    update gitignore
    move dependencies to version catalog

--- src/androidUnitTest/kotlin/com/liftric/cognito/idp/IdentityProviderClientTests.kt
+++ src/androidUnitTest/kotlin/com/liftric/cognito/idp/IdentityProviderClientTests.kt
@@ -1,7 +1,7 @@
 package com.liftric.cognito.idp
 
 import com.bastiaanjansen.otp.HMACAlgorithm
-import com.bastiaanjansen.otp.TOTP
+import com.bastiaanjansen.otp.TOTPGenerator
 import kotlinx.coroutines.runBlocking
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -10,10 +10,12 @@ import java.time.Duration
 @RunWith(RobolectricTestRunner::class)
 actual class IdentityProviderClientTests: AbstractIdentityProviderClientTests() {
     override fun generateTotpCode(secret: String): String? {
-        val builder = TOTP.Builder(secret.toByteArray())
+        val builder = TOTPGenerator.Builder(secret.toByteArray())
         builder
-            .withPasswordLength(6)
-            .withAlgorithm(HMACAlgorithm.SHA1) // SHA256 and SHA512 are also supported
+            .withHOTPGenerator { hotpGenerator ->
+                hotpGenerator.withPasswordLength(6)
+                hotpGenerator.withAlgorithm(HMACAlgorithm.SHA1) // SHA256 and SHA512 are also supported
+            }
             .withPeriod(Duration.ofSeconds(30))
 
         val totp = builder.build()
